be983ea038eb5f68cb8ca54256b8dfd7461e6fcf
Viswanath Atlu <viswanath.700@gmail.com>
2018-04-22 18:18:23 -0600

    Added blocker stuff.

--- Assets/Game/Scripts/Blocker.cs
+++ Assets/Game/Scripts/Blocker.cs
@@ -4,5 +4,73 @@ using UnityEngine;
 
 public class Blocker : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer _sprite;
+    [SerializeField] private float _minAlpha;
+    [SerializeField] private float _maxAlpha;
+    [SerializeField] private float _pulseDuration;
 
+    private bool _isBlocking;
+    public bool IsBlocking
+    {
+        get { return _isBlocking; }
+        set
+        {
+            _isBlocking = value;
+            SetSpriteState();
+        }
+    }
+
+    private void Awake()
+    {
+        IsBlocking = false;
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("enter");
+    }
+    private void OnMouseExit()
+    {
+        Debug.Log("exit");
+    }
+
+    private void OnMouseDown()
+    {
+        IsBlocking = true;
+    }
+
+    private void SetSpriteState()
+    {
+        if (IsBlocking)
+        {
+            iTween.Stop(gameObject);
+            SetSpriteAlpha(1f);
+        }
+        else FadeOut();
+    }
+
+    private void FadeOut()
+    {
+        iTween.Stop(gameObject);
+        iTween.ValueTo(gameObject, iTween.Hash(
+            "from", _maxAlpha, "to", _minAlpha,
+            "time", _pulseDuration, "easetype", "linear",
+            "onupdate", "SetSpriteAlpha", "oncomplete", "FadeIn"));
+    }
+
+    private void FadeIn()
+    {
+        iTween.Stop(gameObject);
+        iTween.ValueTo(gameObject, iTween.Hash(
+            "from", _minAlpha, "to", _maxAlpha,
+            "time", _pulseDuration, "easetype", "linear",
+            "onupdate", "SetSpriteAlpha", "oncomplete", "FadeOut"));
+    }
+
+    private void SetSpriteAlpha(float alpha)
+    {
+        var color = _sprite.color;
+        color.a = alpha;
+        _sprite.color = color;
+    }
 }
---
+++ Assets/Game/Scripts/DroneBlocker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneBlocker : MonoBehaviour
+{
+    private List<Blocker> _blockers = new List<Blocker>();
+
+    private void Awake()
+    {
+        LoadBlockers();
+    }
+
+    private void LoadBlockers()
+    {
+        _blockers.Clear();
+        foreach (Blocker blocker in transform)
+        {
+            _blockers.Add(blocker);
+        }
+    }
+
+
+}
